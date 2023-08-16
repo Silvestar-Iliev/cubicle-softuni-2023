94ab91286c68095e07167006740b485837b49d06
Silvestar-Iliev <stivigerardd@gmail.com>
2023-08-16 20:52:14 +0300

    Add express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from express')
+});
+
+app.listen(PORT, () => console.log(`server ${PORT}`))
